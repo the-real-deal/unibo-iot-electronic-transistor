50dd33c8bbc3a4acd7390538aab5289cf518640a
Gioele Foschi <gioele.foschi.14@gmail.com>
2026-05-27 18:03:10 +0200

    Created server agents

---
+++ Assignment-03/CUS/src/main/java/it/unibo/iot/Agents/SerialAgent.java
@@ -0,0 +1,13 @@
+package it.unibo.iot.Agents;
+
+import io.vertx.core.Future;
+import io.vertx.core.VerticleBase;
+
+public class SerialAgent extends VerticleBase {
+
+    @Override
+    public Future<?> start() throws Exception {
+        return super.start();
+    }
+
+}
