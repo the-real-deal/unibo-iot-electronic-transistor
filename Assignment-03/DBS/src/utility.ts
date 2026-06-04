179988d7c3a5422d240431dca736d094a3477e47
Gioele Foschi <gioele.foschi.14@gmail.com>
2026-06-04 09:30:30 +0200

    More comments

--- Assignment-03/DBS/src/utility.ts
+++ Assignment-03/DBS/src/utility.ts
@@ -2,6 +2,11 @@ import type { Mode } from "./data";
 
 const VALID_MODES: Mode[] = ['automatic', 'manual', 'unconnected', 'not_available'];
 
+/**
+ * Checks if the string value is a valid state
+ * @param value The string literal
+ * @returns true if the string is a valid mode
+ */
 export function isMode(value: string): value is Mode {
     return VALID_MODES.includes(value as Mode);
 }
